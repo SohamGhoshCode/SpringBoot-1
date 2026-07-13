af298bc86f174423bb90bb23aa9e800d1208f34c
Soham Ghosh <2004sohamghosh@gmail.com>
2026-07-13 11:31:10 +0530

    Spring Boot-1

---
+++ src/main/java/com/soham/demo/DemoApplication.java
@@ -0,0 +1,17 @@
+package com.soham.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+
+		Student student = new Student(22, "Soham");
+
+		System.out.println(student.getAge());
+		System.out.println(student.getName());
+	}
+}
